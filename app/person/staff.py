74d6886e66d6c4580b8789678199bee183ccd745
Bonny <boniface.mwenda@andela.com>
2016-11-13 17:08:11 +0300

    Created Staff class

---
+++ app/person/staff.py
@@ -0,0 +1,10 @@
+class Staff():
+    """
+    Staff class
+
+    Attributes:
+    category - sets the staff's role to staff
+    """
+    def __init__(self, name):
+        self.name = name
+        self.category = 'staff'
