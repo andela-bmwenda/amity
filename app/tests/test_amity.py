42b48d4d8c77af287f4fe0d507e9c02503b84b4a
Bonny <boniface.mwenda@andela.com>
2016-11-28 08:02:08 +0300

    Added test for print room

---
+++ app/tests/test_amity.py
@@ -0,0 +1,20 @@
+import unittest
+from app.amity.amity import Amity
+
+
+class TestAmityClass(unittest.TestCase):
+    """Tests for Amity class"""
+
+    def test_load_state(self):
+        """Test that data is loaded to the app from db"""
+
+        pass
+
+    def test_save_state(self):
+        """Test that data can be saved to db"""
+
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
