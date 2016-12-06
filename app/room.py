0ac45656e3b76564d1e1752dd16ae91cfc918134
Boniface Mwenda <boniface.mwenda@andela.com>
2016-12-06 07:14:40 +0300

    Set room maximum capacity

--- app/room.py
+++ app/room.py
@@ -23,5 +23,4 @@ class Office(Room):
 
     def __init__(self, room_name):
         super(Office, self).__init__(
-            room_name, room_type="Office", capacity=4, occupants=[])
-
+            room_name, room_type="Office", capacity=6, occupants=[])
