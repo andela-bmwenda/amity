93b368507f8adda4c31305c7daacd0db0ac0360a
Boniface Mwenda <boniface.mwenda@andela.com>
2016-12-06 07:12:51 +0300

    Added reallocate person method

--- app/amity.py
+++ app/amity.py
@@ -1,5 +1,5 @@
 import random
-
+import os
 from app.person import Fellow, Staff
 from app.room import Office, LivingSpace
 
@@ -19,18 +19,14 @@ class Amity(object):
     """
 
     unallocated_members = []
-    allocated_members = {}
+    allocated_members = []
     list_of_rooms = []
 
     def __init__(self):
         pass
 
-    def add_person(self, first_name, last_name, role, wants_accomodation='N'):
+    def add_person(self, person_name, role, wants_accomodation='N'):
 
-        # check if person already exists.
-        # check that the category is valid.
-        # if above checks pass, add person to unallocated_members dictionary.
-        # check that there are available rooms. if no, raise alert and break.
         # check that there are available rooms suitable for the person. if no, raise alert.
         # if yes, if person is a fellow and wants_accomodation = 'Y', allocate
         # them a random living space.
@@ -38,76 +34,137 @@ class Amity(object):
         # if person is staff/wants_accomodation = 'N',
         # allocate them a random office.
         # how to allocate a room:
-        # get the room_name(key) from rooms dic. Append person_name to list_of_occupants
-        # if member is successfully allocated a room, transfer them
-        # from unallocated_members dictionary to allocated_members.
-        # Key = person_name, value = room eg {John: Closure}
-        # if room allocation is unsuccessful, alert the user.
-        # return message:
-        # person_name allocated to room_name
-        # Create a person instance and their identifier
 
+        person = [
+            person for person in Amity.allocated_members
+            if person_name.upper() == person.person_name.upper() and
+            person.role.upper() == role.upper()
+        ]
+        if person:
+            print("Warning! {0} exists in amity. ID: {1}".format(
+                person_name, person[0].identifier)
+            )
+            ans = input("Is this a different person? y/n\n")
+            if ans == 'y':
+                pass
+            else:
+                return
         if role.upper() == "STAFF":
-            person = Staff(first_name, last_name)
+            person = Staff(person_name)
         elif role.upper() == "FELLOW":
-            person = Fellow(first_name, last_name, wants_accomodation)
+            person = Fellow(person_name, wants_accomodation)
         else:
             print("Invalid person role: " + role)
             return
+        self._allocate_room(person, wants_accomodation)
 
-        allocated = False
-        while not allocated:
-            # Check if there are rooms
-            if len(Amity.list_of_rooms):
-                # Allocate person a room randomly
-                room = random.choice(Amity.list_of_rooms)
-                if room.room_type == "Living Space":
-                    if room.capacity < 5:
-                        room.occupants.append(person)
-                        allocated = True
-                        break
-                elif room.room_type == "Office":
-                    if room.capacity < 7:
-                        room.occupants.append(person)
-                        allocated = True
-                        break
+    def _allocate_room(self, person, wants_accomodation):
 
+        person = person
+        wants_accomodation = wants_accomodation
+        # Check if there are rooms
+        if len(Amity.list_of_rooms):
+            # Make a list of available rooms depending on person role
+            available_rooms = []
+            for room in Amity.list_of_rooms:
+                if (room.room_type == "Office" and len(room.occupants) < 6) \
+                        or (room.room_type == "Living Space" and len(room.occupants) < 4):
+                    available_rooms.append(room)
+
+            if person.role == "Staff":
+                available_rooms = [room for room in available_rooms if room.room_type == "Office"
+                                   and len(room.occupants) < 6]
+            if available_rooms:
+                room = random.choice(available_rooms)
             else:
-                print("No rooms available")
-                break
+                print("sorry, we don't have available rooms at the moment")
+            allocated = False
+            while not allocated:
+                # Allocate person a room randomly
+                if room.room_type == "Living Space" and len(room.occupants) < 4 \
+                        and person.role == "Fellow" and wants_accomodation == 'Y':
+                    room.occupants.append(person)
+                    allocated = True
+                    break
+                elif room.room_type == "Living Space" and len(room.occupants) < 4 \
+                        and person.role == "Fellow":
+                    room.occupants.append(person)
+                    allocated = True
+                    break
+                elif room.room_type == "Office" and len(room.occupants) < 6:
+                    room.occupants.append(person)
+                    allocated = True
+                    break
+                else:
+                    print("Sorry. We could not find you a room at this moment")
+                    break
+        else:
+            print("There are no rooms to allocate")
+
         if not allocated:
             Amity.unallocated_members.append(person)
             print("{} was added to the waiting list".format(
-                person.first_name))
+                person.person_name))
         else:
+            Amity.allocated_members.append(person)
             print("{} successfully allocated {}".format(
-                person.first_name, room.room_name))
+                person.person_name, room.room_name))
 
-    def reallocate_person(self, person_name, new_room_name):
+    def reallocate_person(self, person_name, room_name):
 
-        # check that person and room exist. Raise alert if necessary
-        # get the person's current room from allocated_members dictionary
-        # call the allocate room function in add_person()
         # if successful, alert the user of the change,
         # eg, you've been reallocated to Java from Swift
         # set the new value of the dic to new_room_name
         # return message:
         # person_name transferred from former_room to new_room
-        pass
+
+        room = [room for room in Amity.list_of_rooms
+                if room_name.upper() == room.room_name.upper()]
+        person = [person for person in Amity.allocated_members
+                  if person_name.upper() == person.person_name.upper()]
+        person, room = person[0], room[0]
+        if not person:
+            print("{} has no room in Amity".format(person_name))
+            return
+        elif not room:
+            print("{} is not a room in Amity".format(room_name))
+            return
+        elif [occupant for occupant in room.occupants
+                if person_name.upper() == occupant.person_name.upper()]:
+            print("{0} is already in {1}".format(
+                person_name, room.room_name))
+            return
+
+        elif room.room_type == "Living Space" and person.role == "Staff":
+            print("{} is a living space. Staff can only be allocated offices"
+                  .format(room.room_name))
+        elif (room.room_type == "Living Space" and len(room.occupants) > 4) \
+                or (room.room_type == "Office" and len(room.occupants) > 6):
+            print("{} is full".format(room.room_name))
+        else:
+            current_room = [room for room in Amity.list_of_rooms
+                            if person in room.occupants]
+            room.occupants.append(person)
+            current_room[0].occupants.remove(person)
+            print("{0} was moved from {1} to {2}".format(
+                person.person_name, current_room[0].room_name, room.room_name))
 
     def print_unallocated(self):
 
-        # get the dictionary of unallocated_members
-        # loop through the dic and print each name
-        # save the data to a txt if specified
-        pass
+        if len(Amity.unallocated_members):
+            print("UNALLOCATED PEOPLE")
+            for person in Amity.unallocated_members:
+                print("\t{}".format(person.person_name))
+        else:
+            print("Awesome! There are no people in the waiting list")
 
     def create_room(self, room_name):
         """Create a new room"""
 
-        # check if the room_type is allowed, return an error if not
-        # check if the room already exists, if yes return an error
-
+        # Check if the room exists
+        if [room for room in Amity.list_of_rooms if room_name.upper() == room.room_name.upper()]:
+            print("{0} already exists in Amity.".format(room_name))
+            return
         r_type = random.choice(["Office", "LivingSpace"])
         if r_type == "Office":
             room = Office(room_name)
@@ -116,8 +173,6 @@ class Amity(object):
         Amity.list_of_rooms.append(room)
         print("Created {0}: Type is {1}".format(
             room.room_name, room.room_type))
-
-        # print("Total rooms: {}".format(len(Amity.list_of_rooms)))
 
     def print_room(self, room_name):
         """Print names of all people in room_name"""
@@ -129,27 +184,27 @@ class Amity(object):
             print("{} OCCUPANTS \n".format(room.room_name.upper()))
             print("-----------------------------------------------------")
             for occupant in room.occupants:
-                print("\t{0} {1}\n".format(
-                    occupant.first_name, occupant.last_name)
-                )
+                print("\t{0}\n".format(occupant.person_name))
         else:
             print("{} does not exist in Amity".format(room_name))
             return
 
-    def print_allocations(self):
+    def print_allocations(self, filename):
 
-        # get the dictionary of rooms from living space and office
-        # for each item, if a room has occupants,
-        # print the key(room_name) and its occupants
-        # if a room has no occupants, skip printing
-        # format the output as required and print
-        for room in Amity.list_of_rooms:
-            print(room.room_name.upper() + '\n')
-            print("-----------------------------------------------------")
-            for occupant in room.occupants:
-                print((occupant.first_name + " " +
-                       occupant.last_name + '\t'), end=" ")
-            print('\n')
+        if not Amity.list_of_rooms:
+            print("No rooms to display")
+            return
+        if filename:
+            path = os.path.join(os.path.dirname(__file__), "filename")
+            with open(path, 'w') as f:
+                for room in Amity.list_of_rooms:
+                    if len(room.occupants):
+                        f.write(room.room_name.upper() + '\n')
+                        f.write(
+                            "------------------------------------------------------")
+                        for occupant in room.occupants:
+                            f.write((occupant.person_name + ','), end=" ")
+                        f.write('\n')
 
     def load_people(self, path):
 
@@ -180,6 +235,4 @@ class Amity(object):
 
 
 if __name__ == '__main__':
-    Amity.create_room("Go", "Swift", "Krypton", "Oculus", "Valhala")
-    Amity.add_person("self", "John", "Doe", "Fellow", 'Y')
-    Amity.add_person("self", "Mary", "Jane", "Staff", 'N')
+    main()
