06c4b587b67b507944c58199f6ba0465d76c6bb7
Boniface Mwenda <boniface.mwenda@andela.com>
2016-12-13 21:07:30 +0300

    Created class variable to count objects total

--- app/person.py
+++ app/person.py
@@ -1,33 +1,34 @@
-import random
-
-
 class Person(object):
-    """
-    This class is the base class for Fellow and Staff.
+    """This class is the base class for Fellow and Staff.
     It defines common attributs below Fellow and Staff
     and initialises them.
     """
 
-    def __init__(self, person_name, role=None):
-        """Initializes Person class"""
+    total = 0
 
-        self.identifier = random.randint(0, 9999)
+    def __init__(self, person_name, role=None):
+        """Initialize Person class."""
+
         self.person_name = person_name
         self.role = role
+        self.current_room = None
+        Person.total += 1
 
 
 class Fellow(Person):
-    """Creates fellows, and inherits from Person class"""
+    """Creates fellows, and inherits from Person class."""
 
     def __init__(self, person_name, wants_accomodation='N'):
         super(Fellow, self).__init__(person_name)
+        self.identifier = Person.total
         self.role = "Fellow"
         self.wants_accomodation = wants_accomodation
 
 
 class Staff(Person):
-    """Creates Staff members and inherits from Person"""
+    """Creates Staff members and inherits from Person."""
 
     def __init__(self, person_name):
         super(Staff, self).__init__(person_name)
+        self.identifier = Person.total
         self.role = "Staff"
