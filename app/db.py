5c0423e8e88618583c0dc0266bd1608baf551ad1
Boniface Mwenda <boniface.mwenda@andela.com>
2016-12-19 16:44:08 +0300

    Created methods to save and load data

--- app/db.py
+++ app/db.py
@@ -1,40 +1,56 @@
 import sqlite3
-conn = sqlite3.connect("amity_db.db")
-global c
-c = conn.cursor()
-
-c.execute("DROP TABLE IF EXISTS rooms")
-c.execute("""CREATE TABLE IF NOT EXISTS rooms
-          (room_name CHAR(10),
-           room_type CHAR(10),
-           capacity INT,
-           occupants TEXT);""")
-
-c.execute("DROP TABLE IF EXISTS persons")
-c.execute("""CREATE TABLE IF NOT EXISTS persons
-          (identifier INT,
-           person_name CHAR(50),
-           role CHAR(10),
-           current_room CHAR(20));""")
-
-c.execute("DROP TABLE IF EXISTS session")
-c.execute("""CREATE TABLE IF NOT EXISTS session
-          (data BLOB);""")
 
 
-def write_data(amity_rooms, allocated, unallocated, state):
-    with conn:
-        c.executemany("INSERT INTO rooms VALUES (?, ?, ?, ?)", amity_rooms)
-        c.executemany("INSERT INTO persons VALUES (?, ?, ?, ?)", allocated)
-        c.executemany("INSERT INTO persons VALUES (?, ?, ?, ?)", unallocated)
-        c.execute("INSERT INTO session VALUES (?)", (state, ))
+class AmityDb(object):
+    """
+    Class that defines methods for writing
+    and reading Amity data to the database
+    """
 
+    def __init__(self, db_name):
+        self.conn = sqlite3.connect(db_name)
+        self.c = self.conn.cursor()
 
-def read_data():
-    with conn:
-        c.execute("SELECT * FROM session ORDER BY data DESC LIMIT 1")
-        try:
-          data = c.fetchone()[0]
-        except Exception:
-          return None
-        return data
+    def create_tables(self):
+        with self.conn:
+            self.c.executescript("""
+                DROP TABLE IF EXISTS rooms;
+                CREATE TABLE IF NOT EXISTS rooms
+                        (room_name CHAR(10),
+                         room_type CHAR(10),
+                         capacity INT,
+                         occupants TEXT
+                         );
+
+                DROP TABLE IF EXISTS persons;
+                CREATE TABLE IF NOT EXISTS persons
+                        (identifier INT,
+                         person_name CHAR(50),
+                         role CHAR(10),
+                         current_room CHAR(20)
+                         );
+
+                DROP TABLE IF EXISTS session;
+                CREATE TABLE IF NOT EXISTS session
+                        (data BLOB);
+
+                """)
+
+    def write_data(self, amity_rooms, allocated, unallocated, state):
+        with self.conn:
+            self.c.executemany(
+                "INSERT INTO rooms VALUES (?, ?, ?, ?)", amity_rooms)
+            self.c.executemany(
+                "INSERT INTO persons VALUES (?, ?, ?, ?)", allocated)
+            self.c.executemany(
+                "INSERT INTO persons VALUES (?, ?, ?, ?)", unallocated)
+            self.c.execute("INSERT INTO session VALUES (?)", (state, ))
+
+    def read_data(self):
+        with self.conn:
+            self.c.execute("SELECT * FROM session ORDER BY data DESC LIMIT 1")
+            try:
+                data = self.c.fetchone()[0]
+            except Exception:
+                return None
+            return data
